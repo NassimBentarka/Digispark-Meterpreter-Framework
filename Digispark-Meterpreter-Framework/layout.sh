f2bd7db97dcbee8196a7af9d85618873f609ea12
Nassim Bentarka <nassimbentarka@gmail.com>
2018-08-20 04:56:50 +0100

    Delete layout.sh

--- Digispark-Meterpreter-Framework/layout.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#!/bin/bash
-
-#az_qw_convert.sh - Enhanced by NBN - (Case sensitive input)
-txt=$1
-#read txt
-echo "$txt" | sed 'y/aAqQwWzZ,?;.:mM/qQaAzZwWmM,<.;:/' | sed "y/!\//\/>/" | sed 'y/&é"(-è_çà)°1234567890^¨$£µ%§/123567890-_!@#$%^&*()[{]}¦"?/' | sed "y/ù'/'4/"
